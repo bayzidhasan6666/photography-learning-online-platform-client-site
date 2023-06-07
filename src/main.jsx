c1076768108e3c4eaa5e964abbf4eb219f03ad63
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-06-07 11:22:38 +0800

    added nav and footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <div className="  max-w-screen-xl lg:mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+);
